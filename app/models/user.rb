bbf215e31b44ef60d76a3e1411591fa4f0329e51
Arnaud Levy <contact@arnaudlevy.com>
2022-06-28 21:50:30 +0200

    users

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,9 +19,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
 
   has_many :logs
 
+  def to_s
+    "#{email}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :themes, :weeks, :years
+  resources :users, only: [:index]
   get 'log' => 'dashboard#log', as: :log
   root "dashboard#index"
 end
